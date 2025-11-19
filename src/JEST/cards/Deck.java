78d53e9a2493f4d75bed9db56babab34d5b88af1
Arthur Dodin <dodinarthur@gmail.com>
2025-11-19 08:41:39 +0100

    feat: decks made of Queues

--- src/JEST/cards/Deck.java
+++ src/JEST/cards/Deck.java
@@ -5,15 +5,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class Deck implements Serializable {
-    private List<Card> cards;
+    private Queue<Card> cards;
     
     private static final Map<DeckType, Deck> instances = new HashMap<>();
     
     private Deck() {
-        this.cards = new ArrayList<>();
+        this.cards = new LinkedList<>();
     }
     
     public static synchronized Deck getInstance(DeckType type) {
@@ -22,47 +24,47 @@ public class Deck implements Serializable {
 
     public void fill() {
         try {
-            cards.add(new JokerCard());
-            cards.add(new SuitCard(Suit.SPADE, 1));
-            cards.add(new SuitCard(Suit.SPADE, 2));
-            cards.add(new SuitCard(Suit.SPADE, 3));
-            cards.add(new SuitCard(Suit.SPADE, 4));
-            cards.add(new SuitCard(Suit.CLUB, 1));
-            cards.add(new SuitCard(Suit.CLUB, 2));
-            cards.add(new SuitCard(Suit.CLUB, 3));
-            cards.add(new SuitCard(Suit.CLUB, 4));
-            cards.add(new SuitCard(Suit.HEART, 1));
-            cards.add(new SuitCard(Suit.HEART, 2));
-            cards.add(new SuitCard(Suit.HEART, 3));
-            cards.add(new SuitCard(Suit.HEART, 4));
-            cards.add(new SuitCard(Suit.DIAMOND, 1));
-            cards.add(new SuitCard(Suit.DIAMOND, 2));
-            cards.add(new SuitCard(Suit.DIAMOND, 3));
-            cards.add(new SuitCard(Suit.DIAMOND, 4));
+            this.cards.add(new JokerCard());
+            this.cards.add(new SuitCard(Suit.SPADE, 1));
+            this.cards.add(new SuitCard(Suit.SPADE, 2));
+            this.cards.add(new SuitCard(Suit.SPADE, 3));
+            this.cards.add(new SuitCard(Suit.SPADE, 4));
+            this.cards.add(new SuitCard(Suit.CLUB, 1));
+            this.cards.add(new SuitCard(Suit.CLUB, 2));
+            this.cards.add(new SuitCard(Suit.CLUB, 3));
+            this.cards.add(new SuitCard(Suit.CLUB, 4));
+            this.cards.add(new SuitCard(Suit.HEART, 1));
+            this.cards.add(new SuitCard(Suit.HEART, 2));
+            this.cards.add(new SuitCard(Suit.HEART, 3));
+            this.cards.add(new SuitCard(Suit.HEART, 4));
+            this.cards.add(new SuitCard(Suit.DIAMOND, 1));
+            this.cards.add(new SuitCard(Suit.DIAMOND, 2));
+            this.cards.add(new SuitCard(Suit.DIAMOND, 3));
+            this.cards.add(new SuitCard(Suit.DIAMOND, 4));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void shuffle() {
-        Collections.shuffle(cards);
+        List<Card> cardsList = new ArrayList<>(this.cards);
+        Collections.shuffle(cardsList);
+        this.cards = new LinkedList<>(cardsList);
     }
 
     public Card deal(int numCards) {
-        // Distribuer une carte
-        return null; // Remplacer par la carte distribuée
+        return this.cards.poll();
     }
 
     public boolean isEmpty() {
-        return cards.isEmpty();
+        return this.cards.isEmpty();
     }
     
     public String toString() {
-        System.out.println(cards.size());
-        for (Card card : cards) {
+        System.out.println(this.cards.size());
+        for (Card card : this.cards) {
             System.out.println(card);
         }
-        //return sb.toString();
-        return "blabla";
+        return null;
     }
 }
